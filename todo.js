672f50c277d9a55210fa67cf231885fa55ef0b12
ADhakar672 <adhakar678gmail.com>
2025-02-16 23:13:11 +0530

    Uploaded New Project To-Do App Repo

---
+++ todo.js
@@ -0,0 +1,40 @@
+let todoList =  [
+    {
+        item: 'Go to',
+        dueDate: '18/02/2025'
+    },
+    {
+        item: 'Milk',
+        dueDate: '17/02/2025',
+    }
+    ];
+
+displayItems();
+
+function addTodo() {
+    let inputElement = document.querySelector('#todo-input');
+    let dateElement = document.querySelector('#todo-date');
+    let todoItem = inputElement.value;
+    let todoDate = dateElement.value;
+    todoList.push({item: todoItem, dueDate: todoDate});
+
+    inputElement.value = '';
+    dateElement.value = '';
+
+    displayItems();
+}
+
+function displayItems() {
+    let containerElement = document.querySelector('.todo-container');
+    let newHtml = '';
+    for(let i = 0; i < todoList.length ; i++){
+        let {item, dueDate} = todoList[i];
+        newHtml += `
+            <span>${item}</span>
+            <span>${dueDate}</span>
+            <button class="btn-delete" onClick="todoList.splice(${i}, 1);
+            displayItems();">Delete</button>
+        `;
+    }
+    containerElement.innerHTML = newHtml;
+}
